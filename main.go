e4075a01d60ff1c388e1e3d69f5077a0e84e0d26
Piotr Piotrowski <piotr.j.piotrowski@gmail.com>
2023-11-19 17:24:24 +0100

    use http.Error for setting http status code and response body

--- main.go
+++ main.go
@@ -26,13 +26,14 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 // Handler for display a new snippet
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
 	// Use r.Method to check whether the request is using POST or not.
-	if r.Method != "POST" {
+	if r.Method != http.MethodPost {
 		// It it's not, use the w.WriteHeader() method to send 405 status code
 		// and the w.Write() method to write a "Method Not Allowed" response body.
 		// We then return from the function so that the subsequent code is not executed.
-		w.Header().Set("Allow", "POST")
-		w.WriteHeader(405)
-		w.Write([]byte("Method Not Allowed"))
+		w.Header().Set("Allow", http.MethodPost)
+		// Use the http.Error() function to send a 405 status code and "Method Not Allowed"
+		// string as the response body.
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 		return
 	}
 	w.Write([]byte("Create a new snippet"))
