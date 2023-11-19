b86dd619488c53c3750bad7e76e894a859a252e7
Piotr Piotrowski <piotr.j.piotrowski@gmail.com>
2023-11-19 17:16:41 +0100

    limit snippetCreate only to POST requests

--- main.go
+++ main.go
@@ -25,6 +25,16 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 
 // Handler for display a new snippet
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	// Use r.Method to check whether the request is using POST or not.
+	if r.Method != "POST" {
+		// It it's not, use the w.WriteHeader() method to send 405 status code
+		// and the w.Write() method to write a "Method Not Allowed" response body.
+		// We then return from the function so that the subsequent code is not executed.
+		w.Header().Set("Allow", "POST")
+		w.WriteHeader(405)
+		w.Write([]byte("Method Not Allowed"))
+		return
+	}
 	w.Write([]byte("Create a new snippet"))
 }
 
